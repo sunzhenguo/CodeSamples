9eb0db963ab3fa90c8a274274f2b885cbddddd52
sunzhenguo <sunzhenguo@live.com>
2014-05-28 15:54:28 +0800

    主要框架完成。

---
+++ CodeSamples/Debugger/Code/OfType.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debugger.Code
+{
+
+
+
+    class OfType : ICode 
+    {
+
+
+
+        public void Start()
+        {
+
+
+            List<object> list = new List<object>();
+            list.Add(new OfType_Class() { Id = 1 });
+            list.Add(new OfType_Class() { Id = 2 });
+            list.Add(new OfType_Class() { Id = 3 });
+
+
+            foreach (OfType_Base r in list.OfType<OfType_Base>()) {
+
+                Console.WriteLine(r.Id);
+            }
+
+
+
+
+            throw new NotImplementedException();
+        }
+
+
+    }
+
+
+
+    public class OfType_Class : OfType_Base
+    {
+
+        public int Name { get; set; }
+
+    }
+
+
+    public class OfType_Base
+    {
+
+
+        public int Id { get; set; }
+
+    }
+
+
+
+
+
+
+}
